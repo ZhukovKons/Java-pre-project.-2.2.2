3f3e9250e36aa6cf746444040dd9c9b62afa490f
ZhukovKons <konstantinzhukov91@bk.ru>
2021-04-28 20:31:58 +0300

    end

--- 2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
+++ 2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
@@ -10,6 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -38,6 +42,19 @@ public class WebConfig implements WebMvcConfigurer {
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
+    }
+
+    @Bean
+    public List <Car> getCarList(){
+        int iniId = 0;
+        List<Car> listCar;
+        listCar = new ArrayList<>();
+        listCar.add(new Car(++iniId, "fist", "001"));
+        listCar.add(new Car(++iniId, "second", "002"));
+        listCar.add(new Car(++iniId, "third", "003"));
+        listCar.add(new Car(++iniId, "fourth", "004"));
+        listCar.add(new Car(++iniId, "fifth", "005"));
+        return listCar;
     }
 
 

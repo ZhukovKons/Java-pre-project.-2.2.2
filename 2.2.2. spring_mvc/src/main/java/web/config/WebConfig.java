827de438369975f8d53e701bb99283de5be83a06
ZhukovKons <konstantinzhukov91@bk.ru>
2021-05-06 08:37:18 +0300

    Сервер работает

--- 2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
+++ 2.2.2. spring_mvc/src/main/java/web/config/WebConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,20 +42,6 @@ public class WebConfig implements WebMvcConfigurer {
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
     }
-
-    @Bean
-    public List <Car> getCarList(){
-        int iniId = 0;
-        List<Car> listCar;
-        listCar = new ArrayList<>();
-        listCar.add(new Car(++iniId, "fist", "001"));
-        listCar.add(new Car(++iniId, "second", "002"));
-        listCar.add(new Car(++iniId, "third", "003"));
-        listCar.add(new Car(++iniId, "fourth", "004"));
-        listCar.add(new Car(++iniId, "fifth", "005"));
-        return listCar;
-    }
-
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
--- 2.2.2. spring_mvc/src/main/java/web/model/User.java
+++ 2.2.2. spring_mvc/src/main/java/web/model/User.java
@@ -1,2 +1,61 @@
-package web.model;public class User {
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
